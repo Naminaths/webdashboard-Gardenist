87bfba623ef16613ad64e6dc59ca015a23ccf065
Naminaths <rhaichan343@gmail.com>
2026-01-20 11:28:36 +0700

    feat: Implement Smart Garden web dashboard with UI, charts, and Firebase integration.

--- RUN_DASHBOARD.bat
+++ RUN_DASHBOARD.bat
@@ -4,4 +4,4 @@ echo Mohon jangan tutup jendela ini selama menggunakan dashboard.
 echo.
 echo Membuka Browser...
 start http://localhost:8000
-python -m http.server 8000
+python -m http.server 8000 --directory public
