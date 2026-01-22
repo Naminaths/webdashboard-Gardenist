29162765fd8b27e45c23742c4427f9bd0c5e97d2
Naminaths <rhaichan343@gmail.com>
2026-01-22 18:03:14 +0700

    feat: Add Firebase initialization and database configuration with environment variables.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBoPQqYYAf2qjNCNur0IqQMiCj-sLyWvOs",
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "webdashboard-gardenist.firebaseapp.com",
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL || "https://webdashboard-gardenist-default-rtdb.firebaseio.com",
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "webdashboard-gardenist",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "webdashboard-gardenist.firebasestorage.app",
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "608799094403",
-    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:608799094403:web:0ac4b0e8ff3b72c660cdb6",
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-VL7W2Z6ECV"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 let app;
